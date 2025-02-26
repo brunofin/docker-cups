8cdd36f14d49a6dbfe19a169399dc454fb084a5e
Bruno Finger <bruno.k.finger@gmail.com>
2025-02-26 19:56:37 +0100

    use autoexpect

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,14 +6,6 @@ if [ ! -f /etc/cups/cupsd.conf ]; then
   cp -rpn /etc/cups-skel/* /etc/cups/
 fi
 
-/usr/bin/expect <<'EOF'
-set timeout -1
-spawn hp-plugin -i
-expect {
-    -re ".*Enter option.*" { send "d\r"; exp_continue }
-    -re ".*Do you accept the license terms for the plug-in.*" { send "y\r"; exp_continue }
-    eof
-}
-EOF
+/usr/bin/expect script.exp
 
 exec "$@"
