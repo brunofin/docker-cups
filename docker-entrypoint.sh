d25770636046dedca46169500238b0f04f52ad5f
Bruno Finger <brunofin@users.noreply.github.com>
2025-02-26 10:49:48 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,16 @@ if [ ! -f /etc/cups/cupsd.conf ]; then
   cp -rpn /etc/cups-skel/* /etc/cups/
 fi
 
+/usr/bin/expect <<'EOF'
+spawn hp-plugin -i
+expect {
+    -re ".*Enter option.*" { send "d\r"; exp_continue }
+    eof
+}
+expect {
+    -re ".*Do you accept the license terms for the plug-in*" { send "y\r"; exp_continue }
+    eof
+}
+EOF
+
 exec "$@"
